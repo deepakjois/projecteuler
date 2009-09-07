148f24970aca2ac3e3109e2eb5d6ac037b14d0f7
Deepak Jois <deepak.jois@gmail.com>
2009-09-07 11:45:30 -0700

    Problem 17, much more elegant

--- p17.hs
+++ p17.hs
@@ -7,7 +7,7 @@
 import Data.Map
 
 numbers ::  Integral a => [a]
-numbers = [1..19] ++  [20,30..100] ++ [1000]
+numbers = [1..19] ++  [20,30..100] ++ [1000,1000000]
 words_num = 
   [ "one",
     "two",
@@ -37,19 +37,27 @@ words_num =
     "eighty",
     "ninety",
     "hundred",
-    "thousand"]
+    "thousand",
+    "million"]
 
-numwordsmap = fromList $ zipWith (,) numbers words_num
+numwordsmap = fromAscList $ zipWith (,) numbers words_num
+
+findNearest n =  last $ fst $ break (\x -> fst x > n) (toAscList numwordsmap)
 
 convert n
-      | n == 0               = ""
-      | n < 21               = numwordsmap ! n
-      | n >= 21 && n < 100   = let mul10 = numwordsmap ! (n - n `mod` 10)
-                                   rem10 = convert (n `mod` 10)
-                               in mul10 ++ " " ++ rem10
-      | n >= 100 && n < 1000 = let mul100 = convert (n `div` 100) ++ " " ++ (numwordsmap ! 100)
-                                   rem100 = if (n `mod` 100) > 0 
-                                              then " and " ++ (convert (n `mod` 100))
-                                              else ""
-                               in mul100 ++ rem100
-      | n == 1000            = "one thousand"
+      | n < 0  = "minus " ++ (convert (abs n))
+      | n == 0 = "zero"
+      | n > 0  = inwordsNearest ++ rest
+                  where  (nnum,nwords) = findNearest n
+                         inwordsNearest = if nnum < 100
+                                            then nwords
+                                            else (convert (n `div` nnum)) ++ " " ++ nwords
+                         balance = n `mod` nnum
+                         rest = if balance == 0
+                                  then ""
+                                  else " " ++ convert balance
+
+main = do
+  let countwords = length $ Prelude.filter (\x -> x /= ' ')  $ concat $ Prelude.map convert [1..1000]
+      countands  = 3 * (length $ Prelude.filter (\x -> x `mod` 100 /= 0) [100..999])
+  print $ countands + countwords

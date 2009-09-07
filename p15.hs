bcb9c22ed0d91e8238ced64a63dec61ca25b79a6
Deepak Jois <deepak.jois@gmail.com>
2009-09-06 22:37:57 -0700

    Problem 15

---
+++ p15.hs
@@ -0,0 +1,40 @@
+import Data.Map
+
+-- fully recursive solution for 2x2 grid
+numPaths2x2 c@(x,y)
+      | c == (1,1) = 2
+      | c == (2,1) = 1
+      | c == (1,2) = 1
+      | x > 2 || y > 2 = 0
+      | otherwise  = numPaths (x+1, y) + numPaths (x, y+1)
+
+-- fully recursive solution. Takes forever
+numPaths c@(x,y) n
+      | c == (n-1,n-1) = 2
+      | c == (n,n-1) = 1
+      | c == (n-1,n) = 1
+      | x > n || y > n = 0
+      | otherwise  = numPaths (x+1, y) n + numPaths (x, y+1) n
+
+-- using Map to memoize results, and generating
+-- co-ordinates in a predetermined order outwards
+-- from the bottom right corner
+numPaths_memoized n = seriesMap ! (0,0)
+                      where 
+                        seriesMap   = foldl calcFromMap (fromList []) (genAllTuples n)
+                        genTuples m n =  Prelude.filter (\p@(x,y) -> x == m || y == m)  [ (x,y) | y <- [n,n-1..m], x <- [n,n-1..m]]
+                        genAllTuples n = concat [genTuples m n | m <- [n,n-1..0]]                        
+                        calcFromMap m c@(x,y)
+                                        | c == (n-1,n-1)   = insert c 2 m
+                                        | c == (n,n-1)     = insert c 1 m
+                                        | c == (n-1,n)     = insert c 1 m
+                                        | c == (n,n)       = insert c 0 m
+                                        | x+1 > n          = insert c (m!(x,y+1)) m -- x overflow, add down count only
+                                        | y+1 > n          = insert c (m!(x+1,y)) m -- y overflow, add right count only
+                                        | otherwise        = insert c sumPaths m    -- add right and down count
+                                                              where
+                                                                sumPaths = m!(x+1,y) + m!(x,y+1)
+                                                
+main = do
+  print $ numPaths_memoized 20
+

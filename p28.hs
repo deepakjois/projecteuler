6ed039ce0700149420c25ded3bb29c0f423fcb50
Deepak Jois <deepak.jois@gmail.com>
2009-09-09 14:46:06 -0700

    Applying formula

---
+++ p28.hs
@@ -0,0 +1,10 @@
+sumAllDiagonals n = foldl (+) 1  [diagonalSum x | x <- [3,5..n]]
+
+
+-- (n^2 - (n-1)^2)/4 = n-1
+-- diagonals are (n^2), (n^2)-(n-1), (n^2)-2*(n-1), (n^2)-3(n-1)
+-- summing and simplifying yeilds formula below
+diagonalSum n = ((4*(n*n)) - (6 * (n-1)))
+
+main = do
+ print $ sumAllDiagonals 1001                    

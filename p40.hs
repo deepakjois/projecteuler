e19ce1ea7ed1e434c80ae4fa603fe809501b2981
Deepak Jois <deepak.jois@gmail.com>
2009-09-10 22:16:24 -0700

    Problem 40. Non Brute Force

---
+++ p40.hs
@@ -0,0 +1,14 @@
+import Data.Char
+
+d n | n < 10     = digitToInt $ (show n) !! 0 
+    | otherwise = digit
+                  where
+                    list       = takeWhile (< n) $ scanl1 (+)  $ map (\x -> 9 * (10^x) * (x+1)) [0..]
+                    numDigits  = (length list) + 1
+                    base       = last list
+                    baseNum    = 10 ^ (numDigits - 1) - 1
+                    (increment, digitPlace)  = (n - base) `quotRem` numDigits
+                    number     = baseNum + increment
+                    digit     = if digitPlace == 0
+                                  then digitToInt $ last (show number)
+                                  else digitToInt $ (show (number + 1)) !! (digitPlace - 1)

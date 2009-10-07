01f96647e403190da8fcb0013188e7df81bef7ba
deepakjois <deepak.jois@gmail.com>
2009-10-07 07:11:42 -0700

    Typo

--- p28.hs
+++ p28.hs
@@ -3,7 +3,7 @@ sumAllDiagonals n = foldl (+) 1  [diagonalSum x | x <- [3,5..n]]
 
 -- (n^2 - (n-1)^2)/4 = n-1
 -- diagonals are (n^2), (n^2)-(n-1), (n^2)-2*(n-1), (n^2)-3(n-1)
--- summing and simplifying yeilds formula below
+-- summing and simplifying yields formula below
 diagonalSum n = ((4*(n*n)) - (6 * (n-1)))
 
 main = do
